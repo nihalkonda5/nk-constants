ae3a58c69be9af8b713c0f63b564b16d5c300a41
Nihal Konda <nihal.konda@sjsu.edu>
2020-12-06 16:09:23 -0800

    First Commit

---
+++ lib/api.d.ts
@@ -0,0 +1,12 @@
+declare const _default: {
+    CUSTOM_ERROR: {
+        UNAUTHORIZED: string;
+        REFRESH_TOKEN_EXPIRED: string;
+        ACCESS_TOKEN_EXPIRED: string;
+        TOKEN_EXPIRED: string;
+        AUTHENTICATION_REQUIRED: string;
+        REFRESH_TOKEN_REQUIRED: string;
+        USER_UNCONFIRMED: string;
+    };
+};
+export default _default;
